cbe706d9304cfa16e85d83a09fd2524f7d91a222
Brushblaster <denis.kaech@gmail.com>
2017-11-28 21:13:17 +0100

    adding basic login screen

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,29 +1,50 @@
 <template>
-  <v-content>
-    <v-container>
+    <v-container fluid grid-list-xl>
+      <v-layout row justify-space-between>
+        <v-flex >
     <h1>Register</h1>
 
     <v-form ref="form">
       <v-text-field
         label="Email"
+        v-model="email"
       ></v-text-field>
       <v-text-field
         label="Password"
+        v-model="password"
       ></v-text-field>
 
       <v-btn
+      @click="register"
       >
-      submit
+      Register
       </v-btn>
       <v-btn>clear</v-btn>
     </v-form>
+        </v-flex>
+      </v-layout>
     </v-container>
-  </v-content>
 </template>
 
 <script>
-export default {
+import AuthenticationService from '@/services/AuthenticationService'
 
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
 }
 </script>
 
